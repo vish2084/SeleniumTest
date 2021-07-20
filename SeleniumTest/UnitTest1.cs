ea0a76ef5f1d184d4272b8364af15b6cb81b0dbc
Vishal1125 <vishal.patil@mangosoft.co.in>
2021-07-20 22:27:01 +0530

    Add project files.

---
+++ SeleniumTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
