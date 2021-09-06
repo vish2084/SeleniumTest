452ef3dd49ba08dd8a3b057d8da6029b81f80f82
Vishal2084 <vish2084@gmail.com>
2021-09-07 02:54:28 +0530

    Added docker support

--- DockerCompose/UnitTest1.cs
+++ DockerCompose/UnitTest1.cs
@@ -1,10 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using System;
+using System.Threading;
 
-
-namespace SeleniumTest
+namespace DockerCompose
 {
     [TestClass]
     public class UnitTest1
@@ -12,10 +13,7 @@ namespace SeleniumTest
         [TestMethod]
         public void TestMethod1()
         {
-            IInterfaceOne i = new ChildClass();
-            i.Add();
-            string remoteUrl = "http://localhost:4446/wd/hub";
-
+            string remoteUrl = "http://localhost:4444/wd/hub";
             var options = new ChromeOptions();
             options.AddAdditionalCapability("version", "", true);
             options.AddAdditionalCapability("platform", "LINUX", true);
@@ -23,6 +21,21 @@ namespace SeleniumTest
             driver.Manage().Window.Maximize();
             //IWebDriver driver = new ChromeDriver(@"C:\SeleniumDrivers\chromedriver_win32");
             driver.Url = "https://www.google.com";
+            Thread.Sleep(10000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string remoteUrl = "http://localhost:4444/wd/hub";
+            var options = new FirefoxOptions();
+            options.AddAdditionalCapability("version", "", true);
+            options.AddAdditionalCapability("platform", "LINUX", true);
+            var driver = new RemoteWebDriver(new Uri(remoteUrl), options);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.com";
+            Thread.Sleep(10000);
             driver.Quit();
         }
     }
---
+++ SeleniumTest/IInterfaceOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    interface IInterfaceOne
+    {
+        public virtual void Add()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+        }
+    }
+}
